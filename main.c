f80fd4a8c2454af35ee1351d00ff496edfa24222
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-01-09 17:39:22 -0300

    implemented functions for the philo's actions

--- main.c
+++ main.c
@@ -6,13 +6,73 @@
 /*   By: sabrifer <sabrifer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:00:59 by sabrifer          #+#    #+#             */
-/*   Updated: 2025/01/09 15:09:17 by sabrifer         ###   ########.fr       */
+/*   Updated: 2025/01/09 17:38:27 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 int		error_program_use(void);
+
+void	take_forks(t_philo *philo)
+{
+	long int	start_time;
+	long int	current_time;
+
+	start_time = philo->table->start_time;
+	current_time = timestamp_in_ms() - start_time;
+	// testing with forks
+	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(&philo->table->mutex_printf);
+	printf("%ld %d has taken a fork\n", current_time, philo->philo_id);
+	printf("%ld %d has taken a fork\n", current_time, philo->philo_id);
+	pthread_mutex_unlock(&philo->table->mutex_printf);
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}
+
+void	eating(t_philo *philo)
+{
+	long int	start_time;
+	long int	current_time;
+
+	start_time = philo->table->start_time;
+	// testing mutex with first printf EATING
+	pthread_mutex_lock(&philo->table->mutex_printf);
+	current_time = timestamp_in_ms() - start_time;
+	printf("%ld %d is eating\n", current_time, philo->philo_id);
+	pthread_mutex_unlock(&philo->table->mutex_printf);
+	usleep(philo->table->time_to_eat);
+}
+
+void	sleeping(t_philo *philo)
+{
+	long int	start_time;
+	long int	current_time;
+
+	start_time = philo->table->start_time;
+	// testing mutex with first printf SLEEPING
+	pthread_mutex_lock(&philo->table->mutex_printf);
+	current_time = timestamp_in_ms() - start_time;
+	printf("%ld %d is sleeping\n", current_time, philo->philo_id);
+	pthread_mutex_unlock(&philo->table->mutex_printf);
+	usleep(philo->table->time_to_sleep);
+}
+
+void	thinking(t_philo *philo)
+{
+	long int	start_time;
+	long int	current_time;
+
+	start_time = philo->table->start_time;
+	// testing mutex with first printf THIKING
+	pthread_mutex_lock(&philo->table->mutex_printf);
+	current_time = timestamp_in_ms() - start_time;
+	printf("%ld %d is thinking\n", current_time, philo->philo_id);
+	pthread_mutex_unlock(&philo->table->mutex_printf);
+	usleep(philo->table->time_to_sleep);
+}
 
 void	*meal_routine(void *var)
 {
@@ -23,14 +83,10 @@ void	*meal_routine(void *var)
 	philo = (t_philo *)var;
 	start_time = philo->table->start_time;
 	current_time = timestamp_in_ms() - start_time;
-	// printf("start time in milliseconds: [%ld]\n", start_time);
-	// usleep(5000);
-	// printf("usleep(5000)\n");
-	// printf("time since start time: [%ld]\n", current_time);
-	pthread_mutex_lock(&philo->table->mutex_printf);
-	printf("[%ld] - philo created [id = %d]\n", current_time, philo->philo_id);
-	usleep(500000);
-	pthread_mutex_unlock(&philo->table->mutex_printf);
+	take_forks(philo);
+	eating(philo);
+	sleeping(philo);
+	thinking(philo);
 	return (NULL);
 }
 
